85518b89ebc19d4bf7e100aba4d1e54c6ab3a917
Jan Syring-Lingenfelder <me@jan-sl.de>
2021-04-03 14:35:56 +0200

    zsh: useful keybindings

---
+++ zsh/custom/bindkeys.zsh
@@ -0,0 +1,8 @@
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+
+
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^e' edit-command-line
